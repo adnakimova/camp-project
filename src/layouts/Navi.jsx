80ec56841d4d925583705c3c5067512d1d8de70c
Aidana <akimovadna@gmail.com>
2022-11-04 17:10:15 +0300

    Page part

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -13,17 +13,12 @@ export default function Navigator() {
                         name='messages'
                     />
                     <Menu.Menu position='right'>
-                        <Dropdown item text='Language'>
-                            <Dropdown.Menu>
-                                <Dropdown.Item>English</Dropdown.Item>
-                                <Dropdown.Item>Russian</Dropdown.Item>
-                                <Dropdown.Item>Spanish</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-
+                        <CartSummary>
                         <Menu.Item>
                             <Button primary>Sign Up</Button>
                         </Menu.Item>
+
+                        </CartSummary>
                     </Menu.Menu>
                 </Container>
 
